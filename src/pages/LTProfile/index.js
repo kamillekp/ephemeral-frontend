d7365164b8906a0e5821d86a6501980a7ca26ed5
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2021-03-15 20:34:18 -0300

    fix verify token

--- src/pages/LTProfile/index.js
+++ src/pages/LTProfile/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import api from '../../Services/api';
 import './style.css';
 
